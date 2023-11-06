f235d41e3fce328ab339af56a17b7864c0c2e065
Manel Issa <issamanel45@gmail.com>
2023-11-06 06:28:51 +0100

    send mail

--- src/main/java/com/dompet/sigopback/service/NotificationServiceImpl.java
+++ src/main/java/com/dompet/sigopback/service/NotificationServiceImpl.java
@@ -12,15 +12,14 @@ import org.springframework.stereotype.Service;
 public class NotificationServiceImpl implements NotificationService{
 
     private JavaMailSender javaMailSender;
-    private final String fromMail = "issamanel05@gmail.com";
+
     public void sendMail(ValidationCode validation) {
         SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(this.fromMail);
         message.setTo(validation.getUser().getEmail());
         message.setSubject("Market Verification Code");
 
         String texte = String.format(
-                "Hello %s, <br /> Your verification code is  %s; Have a nice day",
+                "Hello %s, <br /> Your verification code is <b> %s; </b>Have a nice day",
                 validation.getUser().getUsername(),
                 validation.getCode()
         );
